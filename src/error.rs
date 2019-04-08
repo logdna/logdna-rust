073e0f82ae4488aa82693b5b8db74851fe2e2cd1
CJP10 <cjpet10@gmail.com>
2019-04-08 16:55:22 -0400

    initial layout of the client module, requires clean up

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,20 @@
 quick_error! {
      #[derive(Debug)]
      pub enum Error {
+        Json(err: serde_json::Error) {
+             from()
+        }
+        Http(err: http::Error) {
+             from()
+        }
+        Hyper(err: hyper::error::Error) {
+             from()
+        }
+        Utf8(err: std::string::FromUtf8Error) {
+             from()
+        }
+        Io(err: std::io::Error) {
+             from()
+        }
      }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -18,75 +18,129 @@
 #[macro_use]
 extern crate quick_error;
 
-use futures::future::{self, Future, IntoFuture};
+use std::sync::Arc;
+
+use flate2::Compression;
+use flate2::write::GzEncoder;
+use futures::future::{self, Err, Future, IntoFuture};
+use futures::stream::Stream;
 use http::header::{ACCEPT_CHARSET, CONTENT_ENCODING};
 use http::Method;
+use http::request::Builder;
 use hyper::{Client as HyperClient, Request, StatusCode};
+use hyper::body::Body;
 use hyper::client::HttpConnector;
 use hyper_rustls::HttpsConnector;
 use rustls::ClientConfig as TlsConfig;
 use serde::{Deserialize, Serialize};
-use serde::de::DeserializeOwned;
+use serde_json::ser::Serializer;
 use tokio::reactor::Handle;
 use tokio::runtime::TaskExecutor;
+
+use crate::error::Error;
 
 /// Contains all the errors types for this crate
 pub mod error;
 pub mod config;
 
-pub type IngestResponse = Box<Future<Item=Response, Error=()> + Send>;
+pub type IngestResponse = Box<Future<Item=Response, Error=Error> + Send + 'static>;
 
 /// Provides an HTTP(s) client for communicating with logdnas ingest api
 pub struct Client {
-    hyper: HyperClient<HttpsConnector<HttpConnector>>,
-//    config: ClientConfig
+    hyper: Arc<HyperClient<HttpsConnector<HttpConnector>>>,
+//    config: ClientConfig,
 }
 
 impl Client {
-    /// construct a new HTTP(s) client from a tokio TaskExecutor and tokio reactor Handle
     pub fn new(exec: TaskExecutor, reactor: Handle) -> Self {
         let http_connector = {
-            // build an http connector that uses our runtime
             let mut connector = HttpConnector::new_with_executor(
                 exec, Some(reactor),
             );
-            connector.enforce_http(false); // enable https urls
+            connector.enforce_http(false); // this is need or https:// urls will error
             connector
         };
-        //construct tls config got https
+
         let tls_config = {
-            let mut cfg = TlsConfig::new(); //create a new tls config
-            // add trust anchors and ct logs to config
+            let mut cfg = TlsConfig::new();
             cfg.root_store.add_server_trust_anchors(&webpki_roots::TLS_SERVER_ROOTS);
             cfg.ct_logs = Some(&ct_logs::LOGS);
             cfg
         };
-        // build https connector from http connector and tls config
+
         let https_connector = HttpsConnector::from((http_connector, tls_config));
 
         Client {
-            hyper: HyperClient::builder().build(https_connector),
+            hyper: Arc::new(HyperClient::builder().build(https_connector)),
         }
     }
 
     /// construct a future that represents a request to the logdna ingest api
-    pub fn make_request<T: Serialize>(&mut self, ingest_body: T) -> IngestResponse {
-        //todo ingestion key
-        let test = future::ok(Request::builder()
+    pub fn send(&self, body: IngestBody) -> IngestResponse {
+        let hyper = self.hyper.clone(); // get a new ref to the hyper client
+        let req = self.build_template(self.new_template(), body)
+            .and_then(move |req|
+                hyper.request(req).map_err(Into::into)
+            )
+            .and_then(move |res| {
+                let status = res.status();
+                res
+                    .into_body()
+                    .map_err(Error::Hyper)
+                    .fold(Vec::new(), |mut vec, chunk| {
+                        vec.extend_from_slice(&*chunk);
+                        future::ok::<_, Error>(vec)
+                    })
+                    .and_then(move |bytes|
+                        String::from_utf8(bytes)
+                            .map_err(Error::Utf8)
+                    )
+                    .map(move |reason| {
+                        if status != 200 {
+                            Response::Failed(status, reason)
+                        } else {
+                            Response::Sent
+                        }
+                    })
+            });
+
+        Box::new(req)
+    }
+
+    fn new_template(&self) -> Builder {
+        let mut b = Request::builder();
+        b
             .method(Method::POST)
             .header(ACCEPT_CHARSET, "utf8")
             .header(CONTENT_ENCODING, "gzip")
-            .header("apiKey", "")
-        ).and_then(|builder| {
-            serde_json::to_string(&ingest_body)
-                .map_err(|_| ())
-                .and_then(move |json| builder.body(json).map_err(|_| ()))
-                .into_future()
-        });
-        unimplemented!()
+            .header("apiKey", "");
+        b
+    }
+
+    fn build_template(&self, mut template: Builder, body: IngestBody) -> Box<Future<Item=Request<Body>, Error=Error> + Send> {
+        if true {
+            Box::new(
+                future::ok(GzEncoder::new(Vec::new(), Compression::default()))
+                    .and_then(move |mut encoder| {
+                        serde_json::to_writer(&mut encoder, &body)
+                            .map(|_| encoder)
+                            .map_err(Error::Json)
+                    })
+                    .and_then(|encoder| encoder.finish().map_err(Into::into))
+                    .and_then(move |vec| template.body(Body::from(vec)).map_err(Into::into))
+            )
+        } else {
+            Box::new(
+                serde_json::to_vec(&body)
+                    .map_err(Error::Json)
+                    .into_future()
+                    .and_then(move |vec| template.body(Body::from(vec)).map_err(Into::into))
+            )
+        }
     }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct IngestBody {}
 
 pub enum Response {
