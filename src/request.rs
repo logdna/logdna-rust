d1ec6a9aa5bfeeb8dd79dc4051786d47e7089d8e
CJP10 <cjpet10@gmail.com>
2019-04-24 13:10:01 -0400

    added documentation for request module

--- src/request.rs
+++ src/request.rs
@@ -13,26 +13,37 @@ use crate::body::IngestBody;
 use crate::error::{RequestError, TemplateError};
 use crate::params::Params;
 
+///type alias for a request used by the client
 pub type IngestRequest = Box<Future<Item=Request<Body>, Error=RequestError> + Send + 'static>;
 
+/// A reusable template to generate requests from
 pub struct RequestTemplate {
+    /// HTTP method, default is POST
     pub method: Method,
+    /// Content charset, default is utf8
     pub charset: HeaderValue,
+    /// Content type, default is application/json
     pub content: HeaderValue,
+    /// Content encoding, default is gzip
     pub encoding: Encoding,
+    /// Http schema, default is https
     pub schema: Schema,
+    /// Host / domain, default is logs.logdna.com
     pub host: String,
+    /// Ingest endpoint, default is /logs/ingest
     pub endpoint: String,
+    /// Query parameters appended to the url
     pub params: Params,
+    /// LogDNA ingestion key
     pub api_key: String,
 }
 
 impl RequestTemplate {
-
+    /// Constructs a new TemplateBuilder
     pub fn builder() -> TemplateBuilder {
         TemplateBuilder::new()
     }
-
+    /// Uses the template to create a new request
     pub fn new_request(&self, body: IngestBody) -> IngestRequest {
         let mut builder = RequestBuilder::new();
 
@@ -55,6 +66,7 @@ impl RequestTemplate {
     }
 }
 
+/// Used to build an instance of a RequestTemplate
 pub struct TemplateBuilder {
     method: Method,
     charset: HeaderValue,
@@ -68,7 +80,15 @@ pub struct TemplateBuilder {
     err: Option<TemplateError>,
 }
 
+/// Represents the encoding to be used when sending an IngestRequest
+#[derive(Debug, Clone)]
+pub enum Encoding {
+    Json,
+    GzipJson(Compression),
+}
+
 impl TemplateBuilder {
+    /// Constructs a new TemplateBuilder
     pub fn new() -> Self {
         Self {
             method: Method::POST,
@@ -83,12 +103,12 @@ impl TemplateBuilder {
             err: None,
         }
     }
-
+    /// Set the method field
     pub fn method<T: Into<Method>>(&mut self, method: T) -> &mut Self {
         self.method = method.into();
         self
     }
-
+    /// Set the charset field
     pub fn charset<T>(&mut self, charset: T) -> &mut Self
         where HeaderValue: HttpTryFrom<T>
     {
@@ -101,7 +121,7 @@ impl TemplateBuilder {
         };
         self
     }
-
+    /// Set the content field
     pub fn content<T>(&mut self, content: T) -> &mut Self
         where HeaderValue: HttpTryFrom<T>
     {
@@ -114,37 +134,37 @@ impl TemplateBuilder {
         };
         self
     }
-
+    /// Set the encoding field
     pub fn encoding<T: Into<Encoding>>(&mut self, encoding: T) -> &mut Self {
         self.encoding = encoding.into();
         self
     }
-
+    /// Set the schema field
     pub fn schema<T: Into<Schema>>(&mut self, schema: T) -> &mut Self {
         self.schema = schema.into();
         self
     }
-
+    /// Set the host field
     pub fn host<T: Into<String>>(&mut self, host: T) -> &mut Self {
         self.host = host.into();
         self
     }
-
+    /// Set the endpoint field
     pub fn endpoint<T: Into<String>>(&mut self, endpoint: T) -> &mut Self {
         self.endpoint = endpoint.into();
         self
     }
-
+    /// Set the api_key field
     pub fn api_key<T: Into<String>>(&mut self, api_key: T) -> &mut Self {
         self.api_key = Some(api_key.into());
         self
     }
-
+    /// Set the params field
     pub fn params<T: Into<Params>>(&mut self, params: T) -> &mut Self {
         self.params = Some(params.into());
         self
     }
-
+    /// Build a RequestTemplate using the current builder
     pub fn build(&mut self) -> Result<RequestTemplate, TemplateError> {
         Ok(RequestTemplate {
             method: self.method.clone(),
@@ -162,12 +182,6 @@ impl TemplateBuilder {
     }
 }
 
-#[derive(Debug, Clone)]
-pub enum Encoding {
-    Json,
-    GzipJson(Compression),
-}
-
 impl Encoding {
     fn set_builder_encoding<'a>(&self, builder: &'a mut RequestBuilder) -> &'a mut RequestBuilder {
         use crate::request::Encoding::*;
@@ -179,6 +193,7 @@ impl Encoding {
     }
 }
 
+/// Represents HTTP vs HTTPS for requests
 #[derive(Debug, Clone)]
 pub enum Schema {
     Http,
